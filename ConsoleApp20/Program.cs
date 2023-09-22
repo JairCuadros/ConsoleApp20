a6768d8d853cd458c0bd69d6836a47d0b2a8e016
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-09-21 22:10:28 -0500

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ingrese la base:");
+int base1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el exponenete:");
+int exponente=Convert.ToInt32(Console.ReadLine());
+
+int resultado = 1;
+
+for (int i = 1; i <= exponente; i++)
+{
+    resultado = resultado * base1;
+}
+
+Console.WriteLine("******************************");
+Console.WriteLine("******************************");
+Console.WriteLine("El resultado es: " + resultado);
+Console.WriteLine("******************************");
+Console.WriteLine("******************************");
